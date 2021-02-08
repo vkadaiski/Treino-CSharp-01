f31dbebbdc09be4895f92337aae9a32ac99809cb
vkadaiski <vkadaiski@gmail.com>
2021-02-08 12:04:10 -0300

    First

---
+++ exe04/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+ /*
+  Desenvolva um algoritmo que solicite a entrada da idade de um determinado usuário,
+   se for menor que 18 anos exiba na cor vermelha “Sem permissão”, 
+   caso seja maior ou  igual a 18 anos exiba na cor verde “Permissão concedida”
+ */
+namespace exe04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Verificar();
+        }
+
+        static void Verificar(){
+
+            int idade;
+
+            Console.WriteLine("Digite sua idade");
+
+            int.TryParse(Console.ReadLine(),out idade);
+
+            if(idade >= 18 ){
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Acesso Permitido");
+            }
+            else{
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Acesso Negado");
+            }
+
+            
+
+
+        }
+    }
+}
---
+++ exe05/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+Desenvolva um algoritmo que solicite a entrada de uma frase, 
+após isto troque todas as letras A ou a por &, porém não utilize o método Replace().
+*/
+
+namespace exe05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            string Frase = "Agora a frase esta sendo manipulada";
+            Console.WriteLine(p.Alterar(Frase));
+        }
+
+        private string Alterar(string Frase){
+
+            if (Frase.Trim().Length > 0){
+                
+                int Total = Frase.Length;
+                int count = 0;
+                var StringBuilder = new StringBuilder(Frase);
+
+                while(count < Total){
+
+                    if (StringBuilder[count]== 'a'){
+
+                        StringBuilder[count] ='&';
+
+                    }
+                    count++;
+
+
+                }
+                Frase = StringBuilder.ToString();
+                return Frase;
+            }
+            else{
+
+                return null;
+
+            }
+
+
+
+
+        }
+    }
+}
